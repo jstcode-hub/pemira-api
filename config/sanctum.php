3c8f4dafd7bfdabf3cd76d2ec06348f8cdcb0087
unknown <anggayunus139@gmail.com>
2024-11-15 23:16:00 +0700

    fix: resolve CORS issue by updating allowed origins in server configuration

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,8 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        'https://pemira-zeta.vercel.app,' . Sanctum::currentApplicationUrlWithPort()
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1' . Sanctum::currentApplicationUrlWithPort()
     ))),
 
 
